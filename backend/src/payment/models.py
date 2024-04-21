e33725319ea8a19ddf0593e72dc156fee2c6dc9d
Volodymyr Lazarev <vovalaz321@gmail.com>
2024-04-21 17:17:17 +0300

    feat: add rest api related files and base models

--- backend/src/payment/models.py
+++ backend/src/payment/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from base.models import CreatedUpdatedAt
+
+
+class Payment(CreatedUpdatedAt):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
--- backend/src/user/models.py
+++ backend/src/user/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from base.models import CreatedUpdatedAt
+
+
+class User(AbstractUser, CreatedUpdatedAt):
+    # Model configuration
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    # Custom object manager
+    # Overridden AbstractUser's fields
+    username = None
+    date_joined = None
+    email = models.EmailField(help_text="User Email", unique=True, max_length=64)
+
+    class Meta:
+        ordering = ("-id",)
+        db_table = "users"
+        abstract = False
+
+    def __str__(self) -> str:
+        return f"<User: #{self.id} {self.email}>"
