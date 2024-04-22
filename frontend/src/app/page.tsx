6ace3c2a9260d3a10fbe451a7be927e1a063cab2
Vadym Tsyhanash <vadym.tsyhanash.longtail@gmail.com>
2024-04-22 21:40:58 +0300

    add extra fields

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,22 +2,10 @@
 
 // import { api } from "~/trpc/server";
 // import { getServerAuthSession } from "~/server/auth";
-import { signIn, signOut, useSession } from "next-auth/react";
-import Link from "next/link";
 import Header from "./_components/header";
 import LoanCalculatorForm from "./_components/loan-calculator-form";
 
 export default function Home() {
-  const { data: session } = useSession();
-
-  // const handleSignOut = async () => {
-  //   await signOut();
-  // };
-
-  // const handleSignIn = async () => {
-  //   await signIn();
-  // };
-
   return (
     <>
       <Header />
