892bf9eba53daf8b6fe16311458da8e8f6e96126
Volodymyr Lazarev <vovalaz321@gmail.com>
2024-06-09 03:38:23 +0300

    feat: add API grade endpoint

--- backend/src/credit/urls.py
+++ backend/src/credit/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-
 from rest_framework.routers import DefaultRouter
 
-from credit.viewsets import CreditViewSet
 from credit.views import CreditTypeListAPIView
+from credit.viewsets import CreditViewSet
 
 router = DefaultRouter()
 
